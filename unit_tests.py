93b3696bd4ec262041e3ecec16891ae83508d8c9
mrugnivenko <48324852+mrugnivenko@users.noreply.github.com>
2019-03-13 22:25:13 +0300

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
 import unittest
-from fizzbuzz import replace
+from fizzbuzz import is_3
+from fizzbuzz import is_5
+from fizzbuzz import is_3_or_5
+from fizzbuzz import func
 
 class TestFizzbuzz(unittest.TestCase):
 	def test_fizzbuzz(self):
-		nums, A = [], []
+		vchod, vichod = [], []
 		with open('tests.txt') as file:
 			for line in file:
 				nums.append(int(line[:line.find(' ')]))
 				A.append(list(line[line.find(' ') + 1:]))
 		for i in range(len(nums)):
-			f = replace(nums[i])
+			f = func(nums[i])
 			self.assertEqual(f, A[i])
 
 if __name__=="__main__":
