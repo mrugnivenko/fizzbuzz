efde57d67843427545336d775f13bc82f370567f
mrugnivenko <48324852+mrugnivenko@users.noreply.github.com>
2019-03-13 22:08:38 +0300

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,37 @@
+def is_5(chislo1):
+    chislo = str(chislo1)
+    if chislo[-1] == '5' or chislo[-1] == '0':
+        return True
+    else:
+        return False
+
+def is_3(chislo1):
+    chislo = str(chislo1)
+    chislo = abs(int(chislo))
+    chislo = str(chislo)
+    summ = 0
+    for simvol in chislo:
+        summ += int(simvol)
+    if summ == 0 or summ == 3 or summ == 6 or summ == 9:
+        return True
+    elif summ == 1 or summ == 2 or summ == 4 or summ == 5 or summ == 7 or summ == 8:
+        return False
+    else:
+        return is_3(summ)
+
+def is_3_5(chislo1):
+    if is_3(chislo1) and is_5(chislo1):
+        return True
+    else:
+        return False
+
+def f(posl):
+    length = len(posl)
+    for i in range(length):
+        if is_3_5(posl[i]):
+            posl[i] = 'fizzbuzz'
+        elif is_3(posl[i]):
+            posl[i] = 'fizz'
+        elif is_5(posl[i]):
+            posl[i] = 'buzz'
+    return posl
